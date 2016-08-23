47dc118041101950c17b1a5024a1470cf8f8a578
harijithkau <harijith.p@kau.in>
2016-08-23 18:35:13 +0530

    Update bst.java

--- bst.java
+++ bst.java
@@ -1,89 +1,113 @@
+//
+// BinarySearchTree.java
+//
+//
+//
 
 public class BinarySearchTree {
 	public static Node root;
-	public BinarySearchTree()	{
+
+	public BinarySearchTree ()	
+	{
 		this.root = null;
 	}
-	public boolean find(int id)	{
+	public boolean find(int id)	
+	{
 		Node current = root;
-		while(current!=null) {
-			if (current.data==id) {
+		while (current != null) {
+			if (current.data == id) {
 				return true;
-			} else if(current.data>id) {
+			} 
+			else if (current.data > id) {
 				current = current.left;
-			} else {
+			} 
+			else {
 				current = current.right;
 			}
 		}
 		return false;
 	}
 
-	public boolean delete(int id) {
+	public boolean delete(int id) 
+	{
 		Node parent = root;
 		Node current = root;
+	
 		boolean isLeftChild = false;
-		while (current.data!=id) {
+	
+		while (current.data != id) {
 			parent = current;
-			if(current.data>id) {
+			if (current.data > id) {
 				isLeftChild = true;
 				current = current.left;
-			} else {
+			} 
+			else {
 				isLeftChild = false;
 				current = current.right;
 			}
-			if (current ==null) {
+			if (current == null) {
 				return false;
 			}
 		}
+
 		//if i am here that means we have found the node
 		//Case 1: if node to be deleted has no children
-		if (current.left==null && current.right==null) {
+
+		if (current.left == null && current.right == null) {
 			if (current==root) {
 				root = null;
 			}
 			if (isLeftChild ==true) {
 				parent.left = null;
-			} else {
+			} 
+			else {
 				parent.right = null;
 			}
 		}
 		//Case 2 : if node to be deleted has only one child
-		else if(current.right==null) {
-			if(current==root){
+		else if (current.right == null) {
+			if (current == root) {
 				root = current.left;
-				} else if (isLeftChild) {
-					parent.left = current.left;
-				} else {
-					parent.right = current.left;
-				}
+			} 
+			else if (isLeftChild) {
+				parent.left = current.left;
+			} 
+			else {
+				parent.right = current.left;
 			}
-			else if(current.left==null) {
-				if(current==root) {
-					root = current.right;
-				} else if(isLeftChild) {
-					parent.left = current.right;
-				} else {
+		}
+		else if (current.left == null) {
+			if (current == root) {
+				root = current.right;
+			} 
+			else if (isLeftChild) {
+				parent.left = current.right;
+			} 
+			else {
 				parent.right = current.right;
 			}
-		} else if(current.left!=null && current.right!=null) {
+		} else if (current.left != null && current.right != null) {
 		//now we have found the minimum element in the right sub tree
 			Node successor = getSuccessor(current);
 			if (current==root) {
 				root = successor;
-			} else if(isLeftChild) {
+			} 
+			else if (isLeftChild) {
 				parent.left = successor;
-			} else {
+			} 
+			else {
 				parent.right = successor;
 			}
 			successor.left = current.left;
 		}
 		return true;
 	}
-	public Node getSuccessor(Node deleleNode){
-		Node successsor =null;
-		Node successsorParent =null;
+
+	public Node getSuccessor(Node deleleNode) {
+		Node successsor = null;
+		Node successsorParent = null;
 		Node current = deleleNode.right;
-		while(current!=null){
+		while (current != null) {
 			successsorParent = successsor;
 			successsor = current;
 			current = current.left;
@@ -91,45 +115,51 @@ public class BinarySearchTree {
 		//check if successor has the right child, it cannot have left child for sure
 		// if it does have the right child, add it to the left of successorParent.
 		// successsorParent
-		if (successsor!=deleleNode.right){
+		if (successsor != deleleNode.right) {
 			successsorParent.left = successsor.right;
 			successsor.right = deleleNode.right;
 		}
 		return successsor;
 	}
-	public void insert(int id){
+
+	public void insert(int id)
+	{
 		Node newNode = new Node(id);
-		if(root==null){
+		if (root == null) {
 			root = newNode;
 			return;
 		}
 		Node current = root;
 		Node parent = null;
-		while(true) {
+		while (true) {
 			parent = current;
-			if(id<current.data){
+			if (id < current.data) {
 				current = current.left;
-				if(current==null){
+				if (current == null) {
 					parent.left = newNode;
 					return;
 				}
-			}else{
+			}
+			else {
 				current = current.right;
-				if(current==null){
+				if (current==null) {
 					parent.right = newNode;
 					return;
 				}
 			}
 		}
 	}
-	public void display(Node root){
-		if(root!=null){
+	
+	public void display(Node root)
+	{
+		if (root!=null) {
 			display(root.left);
 			System.out.print(" " + root.data);
 			display(root.right);
 		}
 	}
-	public static void main(String arg[]){
+
+	public static void main(String arg[]) {
 		BinarySearchTree b = new BinarySearchTree();
 		b.insert(3);b.insert(8);
 		b.insert(1);b.insert(4);b.insert(6);b.insert(2);b.insert(10);b.insert(9);
@@ -147,11 +177,12 @@ public class BinarySearchTree {
 	}
 }
 
-class Node{
+class Node {
 	int data;
 	Node left;
 	Node right;
-	public Node(int data){
+	public Node (int data)
+	{
 		this.data = data;
 		left = null;
 		right = null;
